96552fe6b4441f022fbaad6f99c44d2426931558
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2021-04-10 21:35:53 +1000

    Add files via upload

---
+++ FAKEBSOD.bat
@@ -0,0 +1,26 @@
+@echo off
+color 17
+echo A problem has been detected and system has been shut down to prevent damage
+echo to your computer
+echo IRQL_NOT_LESS_OR_EQUAL
+echo If this is the first time you've seen this Stop error screen,
+echo restart your computer. If this screen appears again, follow
+echo these steps:
+echo Check to make sure any new hardware or software is properly installed.
+echo If this is a new installation, ask your hardware or software manufactuer
+echo for any system updates you might need.
+echo If problems continue, disable or remove any newly installed hardware
+echo or software. Disable BIOS memory options such as caching or shadowing.
+echo If you need to use Safe Mode to remove or disable components, restart
+echo your computer , press F8 to select Advanced Startup options, and then
+echo select Safe Mode.
+echo Technical information:
+
+echo *** STOP: 0x000000A (0x00000000000000000004A, 0x0000000000000002, 0x00000000000000001,
+echo 0xFFFFF80002B37ABF
+
+echo Collecting data for crash dump...
+echo Initalizing disk for crash dump...
+echo Beginning dump of physical memory.
+echo Dumping physical memory to disk: 95
+pause
